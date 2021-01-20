3231a29437c30f9763827c1a0135ac8b93536afe
Ramon Ojeda <reof1922@gmail.com>
2021-01-20 18:01:39 -0430

    create url and views for store

--- stores/views.py
+++ stores/views.py
@@ -7,9 +7,34 @@ El import order in a Django project is:
 '''
 # Imports from core Django.
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
+
+
+from .models import Store
 
 # Create your views here.
+
+class StoreList(ListView):
+
+    model= Store
+    template_name = 'store_list.html'
+    context_object_name = 'stores'
+    paginate_by = 1
+
+
+class DetailsStore(DetailView):
+
+    model = Store
+    template_name = 'stores/store_details.html'
+    context_object_name  = 'details' #
+    slug_field = 'name_store' #
+
+
 
 # Example view.
 def index(request):
     return render(request,'stores/index.html')
+
+def stores(request):
+    return HttpResponse('aqui una lista de tiendas')
